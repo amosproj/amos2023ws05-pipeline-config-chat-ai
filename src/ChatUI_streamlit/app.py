f0082e1925a2b5eaed07226c67751ededd033eb1
lyndanajjar <lyndanajjar15@gmail.com>
2023-11-28 18:38:58 +0100

    Create chat history section on the left sidebar: - Allow users to review and select from past conversations. - Implement an option to use the user's input prompt as the conversation title.
    Remove unnecessary features:
    - Model selection and temperature adjustment are now removed for a streamlined UI.
    
    Signed-off-by: lyndanajjar <lyndanajjar15@gmail.com>

--- src/ChatUI_streamlit/app.py
+++ src/ChatUI_streamlit/app.py
@@ -3,8 +3,7 @@ import replicate
 import os
 
 # App title
-st.set_page_config(page_title="Pipeline Generation Chatbot")
-
+st.set_page_config(page_title="RTDIP Chatbot")
 
 # Replicate Credentials
 with st.sidebar:
@@ -14,62 +13,75 @@ with st.sidebar:
         replicate_api = st.secrets['REPLICATE_API_TOKEN']
     else:
         replicate_api = st.text_input('Enter Replicate API token:', type='password')
-        if not (replicate_api.startswith('r8_') and len(replicate_api)==40):
+        if not (replicate_api.startswith('r8_') and len(replicate_api) == 40):
             st.warning('Please enter your credentials!', icon='⚠️')
         else:
             st.success('Proceed to entering your prompt message!', icon='👉')
     os.environ['REPLICATE_API_TOKEN'] = replicate_api
 
-    st.subheader('Models and parameters')
-    selected_model = st.sidebar.selectbox('Choose a Llama2 model', ['Llama2-7B', 'Llama2-13B'], key='selected_model')
-    if selected_model == 'Llama2-7B':
-        llm = 'a16z-infra/llama7b-v2-chat:4f0a4744c7295c024a1de15e1a63c880d3da035fa1f49bfd344fe076074c8eea'
-    elif selected_model == 'Llama2-13B':
-        llm = 'a16z-infra/llama13b-v2-chat:df7690f1994d94e96ad9d568eac121aecf50684a0b0963b25a41cc40061269e5'
-    temperature = st.sidebar.slider('temperature', min_value=0.01, max_value=5.0, value=0.1, step=0.01)
-    top_p = st.sidebar.slider('top_p', min_value=0.01, max_value=1.0, value=0.9, step=0.01)
-    max_length = st.sidebar.slider('max_length', min_value=32, max_value=128, value=120, step=8)
-    st.markdown('📖 Learn how to build this app in this [blog](https://blog.streamlit.io/how-to-build-a-llama-2-chatbot/)!')
-
-
 # Store LLM generated responses
-if "messages" not in st.session_state.keys():
-    st.session_state.messages = [{"role": "assistant", "content": "How may I assist you today?"}]
+if "conversations" not in st.session_state.keys():
+    st.session_state.conversations = [{"title": "Default Conversation", "messages": [{"role": "assistant", "content": "How may I assist you today?"}]}]
 
+# Chat history on the left
+st.sidebar.subheader('Chat History')
+
+# Button to load previous conversations
+if st.sidebar.button('Load Previous Conversations'):
+    st.sidebar.text('Select a conversation to open:')
+    selected_conversation = st.sidebar.selectbox('', range(len(st.session_state.conversations)), format_func=lambda x: st.session_state.conversations[x]["title"])
+
+    # Display the selected conversation
+    conversation = st.session_state.conversations[selected_conversation]
+    for message in conversation["messages"]:
+        with st.expander(conversation["title"]):
+            with st.chat_message(message["role"]):
+                st.write(message["content"])
 
 # Display or clear chat messages
-for message in st.session_state.messages:
-    with st.chat_message(message["role"]):
-        st.write(message["content"])
+for conversation in st.session_state.conversations:
+    for message in conversation["messages"]:
+        with st.chat_message(message["role"]):
+            st.write(message["content"])
 
 def clear_chat_history():
-    st.session_state.messages = [{"role": "assistant", "content": "How may I assist you today?"}]
+    st.session_state.conversations = [{"title": "Default Conversation", "messages": [{"role": "assistant", "content": "How may I assist you today?"}]}]
 st.sidebar.button('Clear Chat History', on_click=clear_chat_history)
-
 
 # Function for generating LLaMA2 response. Refactored from https://github.com/a16z-infra/llama2-chatbot
 def generate_llama2_response(prompt_input):
     string_dialogue = "You are a helpful assistant. You do not respond as 'User' or pretend to be 'User'. You only respond once as 'Assistant'."
-    for dict_message in st.session_state.messages:
-        if dict_message["role"] == "user":
-            string_dialogue += "User: " + dict_message["content"] + "\n\n"
-        else:
-            string_dialogue += "Assistant: " + dict_message["content"] + "\n\n"
-    output = replicate.run('a16z-infra/llama13b-v2-chat:df7690f1994d94e96ad9d568eac121aecf50684a0b0963b25a41cc40061269e5', 
-                           input={"prompt": f"{string_dialogue} {prompt_input} Assistant: ",
-                                  "temperature":temperature, "top_p":top_p, "max_length":max_length, "repetition_penalty":1})
-    return output
+    for conversation in st.session_state.conversations:
+        for dict_message in conversation["messages"]:
+            role = dict_message["role"]
+            content = dict_message["content"]
+            if role == "user":
+                string_dialogue += f"User: {content}\n\n"
+            else:
+                string_dialogue += f"Assistant: {content}\n\n"
 
+    output = replicate.run(
+        'a16z-infra/llama13b-v2-chat:df7690f1994d94e96ad9d568eac121aecf50684a0b0963b25a41cc40061269e5',
+        input={
+            "prompt": f"{string_dialogue} {prompt_input} Assistant: ",
+            "repetition_penalty": 1
+        }
+    )
+    return output
 
 # User-provided prompt
 if prompt := st.chat_input(disabled=not replicate_api):
-    st.session_state.messages.append({"role": "user", "content": prompt})
+    # Use the user's prompt as the title without modification
+    title = prompt
+    
+    st.session_state.conversations[-1]["title"] = title
+    st.session_state.conversations[-1]["messages"].append({"role": "user", "content": prompt})
+    
     with st.chat_message("user"):
         st.write(prompt)
 
-
-# Generate a new response if last message is not from assistant
-if st.session_state.messages[-1]["role"] != "assistant":
+# Generate a new response if the last message is not from the assistant
+if st.session_state.conversations[-1]["messages"][-1]["role"] != "assistant":
     with st.chat_message("assistant"):
         with st.spinner("Thinking..."):
             response = generate_llama2_response(prompt)
@@ -79,5 +91,6 @@ if st.session_state.messages[-1]["role"] != "assistant":
                 full_response += item
                 placeholder.markdown(full_response)
             placeholder.markdown(full_response)
+    
     message = {"role": "assistant", "content": full_response}
-    st.session_state.messages.append(message)
+    st.session_state.conversations[-1]["messages"].append(message)
