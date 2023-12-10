de3a0c881943d7b895fc04fca3a7569c3bc1dc99
lyndanajjar <lyndanajjar15@gmail.com>
2023-12-10 12:59:31 +0100

    clean up the repository
    Signed-off-by: lyndanajjar <lyndanajjar15@gmail.com>

--- src/chat-ui/src/lib/types/MessageEvent.ts
+++
@@ -1,7 +0,0 @@
-import type { Timestamps } from "./Timestamps";
-import type { User } from "./User";
-
-export interface MessageEvent extends Pick<Timestamps, "createdAt"> {
-	userId: User["_id"] | User["sessionId"];
-	ip?: string;
-}
