9efee37ed07284b57da7df8ca2fa3898cdece828
BergZain <50025962+bergzain@users.noreply.github.com>
2024-01-08 21:12:26 +0100

    storing the vectore store offfline for faster response time

--- src/ChatUI_streamlit/LLMModel.py
+++ src/ChatUI_streamlit/LLMModel.py
@@ -1,3 +1,4 @@
+#%%
 import os
 from langchain.chains import RetrievalQA
 from langchain.cache import InMemoryCache
@@ -16,8 +17,12 @@ from langchain.document_loaders.text import TextLoader
 from langchain.agents import AgentType, Tool, initialize_agent
 from langchain.memory import ConversationBufferMemory
 from langchain.agents import AgentExecutor
+# import faiss
+from langchain.vectorstores import FAISS as FAISS
+import faiss 
 
 # Load the OpenAI API key
+
 openai_api_key = os.environ["OPENAI_API_KEY"]
 assert openai_api_key is not None, "Failed to load the OpenAI API key from .env file. Please create .env file and add OPENAI_API_KEY = 'your key'"
 
@@ -27,20 +32,28 @@ llm = ChatOpenAI(model_name='gpt-3.5-turbo', openai_api_key=openai_api_key)
 # Load the embeddings
 embeddings = OpenAIEmbeddings(disallowed_special=(), openai_api_key=openai_api_key)
 
-# Load and split documents
-root_dir = '/Users/zainhazzouri/projects/amos2023ws05-pipeline-config-chat-ai/src/RAG/pipelines'
-docs = []
-for dirpath, dirnames, filenames in os.walk(root_dir):
-    for file in filenames:
-        try:
-            loader = TextLoader(os.path.join(dirpath, file), encoding='utf-8')
-            docs.extend(loader.load_and_split())
-        except Exception as e:
-            pass  # Consider logging the exception for debugging
+# # Load and split documents
+# root_dir = '/Users/zainhazzouri/projects/amos2023ws05-pipeline-config-chat-ai/src/RAG/pipelines'
+# docs = []
+# for dirpath, dirnames, filenames in os.walk(root_dir):
+#     for file in filenames:
+#         try:
+#             loader = TextLoader(os.path.join(dirpath, file), encoding='utf-8')
+#             docs.extend(loader.load_and_split())
+#         except Exception as e:
+#             pass  # Consider logging the exception for debugging
 
-# Create the FAISS index
-docsearch = FAISS.from_documents(docs, embeddings)
+# # Create the FAISS index
+# docsearch = FAISS.from_documents(docs, embeddings)
 
+#%%
+# save the vector store offline for later use
+# faiss.write_index(docsearch.index, '/Users/zainhazzouri/projects/amos2023ws05-pipeline-config-chat-ai/src/ChatUI_streamlit/faiss_index_file')
+# docsearch.save_local("/Users/zainhazzouri/projects/amos2023ws05-pipeline-config-chat-ai/src/ChatUI_streamlit/faiss_index")
+
+#%%
+docsearch = FAISS.load_local("/Users/zainhazzouri/projects/amos2023ws05-pipeline-config-chat-ai/src/ChatUI_streamlit/faiss_index", embeddings)
+#%%
 # Initialize RetrievalQA
 RAG = RetrievalQA.from_chain_type(llm, chain_type="stuff", retriever=docsearch.as_retriever())
 
@@ -78,3 +91,5 @@ def update_and_get_context(user_input, conversation_memory):
 # print(response)
 
 # Note: You can uncomment and modify the testing code as per your use case.
+
+# %%
