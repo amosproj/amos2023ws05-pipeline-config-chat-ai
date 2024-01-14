819d1b503921baefb6b2261aefda14b13b6a65da
BergZain <50025962+bergzain@users.noreply.github.com>
2024-01-14 21:44:35 +0100

    implemented python splitter to take heads into consideration Signed-off-by: BergZain <50025962+bergzain@users.noreply.github.com> co-authored-by: BergZain <50025962+bergzain@users.noreply.github.com> co-authored-by: Oneidah Smadi <obeidah0smadi@gmail.com>

--- src/ChatUI_streamlit/LLMModel.py
+++ src/ChatUI_streamlit/LLMModel.py
@@ -85,6 +85,11 @@ documents = loader.load()
 len(documents)
 #%%
 
+python_splitter = RecursiveCharacterTextSplitter.from_language(
+    language=Language.PYTHON, chunk_size=2000, chunk_overlap=200
+)
+texts = python_splitter.split_documents(documents)
+len(texts)
 
 
 #%%
