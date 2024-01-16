0806543114242f5ba484d91a8d1586c811c48eeb
Obeidah Smadi <Obeidah0smadi@gmail.com>
2024-01-16 23:08:39 +0100

    Integrated api storing with the updated main branch Co-authored-by: Zain Hazzouri <50025962+bergzain@users.noreply.github.com>
    Signed-off-by: Obeidah Smadi <Obeidah0smadi@gmail.com>

--- src/ChatUI_streamlit/app.py
+++ src/ChatUI_streamlit/app.py
@@ -2,11 +2,24 @@ import streamlit as st
 import os
 import time
 import requests
-from dotenv import load_dotenv
+import subprocess
+from datetime import datetime
 from pathlib import Path
+from dotenv import load_dotenv
 
 class InvalidAPIKeyException(Exception):
     pass
+
+# Function to check API key validity
+def is_valid_api_key(key):
+    url = "https://api.openai.com/v1/models/gpt-3.5-turbo-instruct"
+    headers = {"Authorization": f"Bearer {key}"}
+    try:
+        response = requests.get(url, headers=headers)
+        return response.status_code == 200
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return False
 
 # Function to load API keys from the openai_keys directory
 def load_api_keys():
@@ -20,33 +33,6 @@ def load_api_keys():
         if api_key:
             api_keys[key_name] = api_key
     return api_keys
-
-# Function to check API key validity
-def is_valid_api_key(key):
-    url = "https://api.openai.com/v1/models/gpt-3.5-turbo-instruct"
-    headers = {"Authorization": f"Bearer {key}"}
-    try:
-        response = requests.get(url, headers=headers)
-        return response.status_code == 200
-    except Exception as e:
-        print(f"An error occurred: {e}")
-        return False
-
-# Initialize page configuration once
-if 'page_config_set' not in st.session_state:
-    st.set_page_config(page_title="RTDIP Pipeline Chatbot")
-    st.session_state['page_config_set'] = True
-
-# HTML/CSS for title and GitHub link
-st.markdown(
-    '''
-    <div style="display: flex; justify-content: space-between; align-items: center;">
-        <h2 style="margin: 0;">RTDIP Pipeline Chatbot</h2>
-        <a href="https://github.com/rtdip/core/tree/develop">
-            <img src="https://img.shields.io/badge/GitHub-Repo-blue?logo=github" alt="GitHub Repo">
-        </a>
-    </div>
-    ''', unsafe_allow_html=True)
 
 # Function to add or select an API key
 def api_key_selection(api_keys):
@@ -65,18 +51,108 @@ def api_key_selection(api_keys):
         else:
             st.sidebar.error('Invalid or missing data for new API Key.')
 
+# Function to check API key and update if necessary
+def check_and_update_api_key():
+    # Check if the OpenAI API key is already stored in the session
+    if 'OPENAI_API_KEY' not in st.session_state:
+        # If not, ask the user to input it
+        openai_api_key = st.text_input('Enter OpenAI API Key:', type='password')
+        if openai_api_key:
+            try:
+                if is_valid_api_key(openai_api_key):
+                    st.session_state['OPENAI_API_KEY'] = openai_api_key
+                    os.environ['OPENAI_API_KEY'] = openai_api_key
+                    st.success('API Key stored!')
+                else:
+                    raise InvalidAPIKeyException
+            except InvalidAPIKeyException:
+                st.error('Invalid OpenAI API Key. Please enter a valid key.')
+
+def run_update_script():
+    script_path = '../UpdateRAG/updateRAG.py'
+    absolute_script_path = os.path.join(os.getcwd(), script_path)
+    command = f'python "{absolute_script_path}"'
+    
+    try:
+        result = subprocess.run(command, shell=True, capture_output=True, text=True, check=True)
+        st.success("Successfully updated RAG.")
+        return result.stdout
+    except subprocess.CalledProcessError as e:
+        st.error(f"Failed to update RAG. Error: {e.stderr}")
+        return e.stderr
+
+
+    
+    
+def get_last_modified_time(folder_path):
+    latest_mod_time = 0
+    for root, _, files in os.walk(folder_path):
+        for file in files:
+            file_path = os.path.join(root, file)
+            try:
+                file_mod_time = os.path.getmtime(file_path)
+                latest_mod_time = max(latest_mod_time, file_mod_time)
+            except Exception as e:
+                pass
+    return datetime.fromtimestamp(latest_mod_time).strftime("%Y-%m-%d") if latest_mod_time else None
+
+
+# Initialize page configuration once
+if 'page_config_set' not in st.session_state:
+    st.set_page_config(page_title="RTDIP Pipeline Chatbot")
+    st.session_state['page_config_set'] = True
+
+# HTML/CSS for title and GitHub link
+st.markdown(
+    '''
+    <div style="display: flex; justify-content: space-between; align-items: center;">
+        <div style="margin-top: -70px; margin-left: -180px;"><h2>RTDIP Pipeline Chatbot</h2></div>
+        <div style="margin-top: -70px; "><a href="https://github.com/rtdip/core/tree/develop"><img src="https://img.shields.io/badge/GitHub-Repo-blue?logo=github"></a></div>
+    </div>
+    ''', unsafe_allow_html=True)
+
+# Load API keys
 api_keys = load_api_keys()
+
+# Add API key selection UI
 api_key_selection(api_keys)
 
-if "conversations" not in st.session_state.keys():
-    st.session_state.conversations = [{"title": "Default Conversation", "messages": [{"role": "assistant", "content": "How may I assist you today?"}]}]
+# Check and update API key
+check_and_update_api_key()
 
-# Check if the API key is set in the session state
+rag_folder_path = os.path.join("..", "RAG")
+
+last_modified_time = get_last_modified_time(rag_folder_path)
+
+left_col, right_col = st.columns([3, 1])  
+
+with right_col:
+    button_style = """
+    <style>
+    .stButton>button {
+       max-width: 200px;  
+    margin-left: 45px; 
+    padding: 5px 10px;
+    border-radius: 15px;
+    }
+    </style>
+    """
+    st.markdown(button_style, unsafe_allow_html=True)
+    if st.button('Update RAG'):
+        run_update_script()
+    st.caption(f"Last update: {last_modified_time}")  
+
+with left_col:
+    st.write("")  # This will create space and push the button and text to the right
+
+if 'conversations' not in st.session_state:
+    st.session_state.conversations = [{"title": "Default Conversation", "messages": [{"role": "assistant", "content": "How may I assist you today?"}]}]
+# Check if the OpenAI API key is set in the session state
 if 'OPENAI_API_KEY' in st.session_state and st.session_state['OPENAI_API_KEY']:
     from LLMModel import initialize_components, update_and_get_context
-    
-    agent, RAG = initialize_components(st.session_state['OPENAI_API_KEY'])
 
+    # Initialize components with OpenAI API key
+    agent, RAG = initialize_components(st.session_state['OPENAI_API_KEY'])
 
     if prompt := st.chat_input():
         conversation = st.session_state.conversations[-1]
@@ -87,6 +163,7 @@ if 'OPENAI_API_KEY' in st.session_state and st.session_state['OPENAI_API_KEY']:
         with st.chat_message("assistant"):
             start_time = time.time()
             with st.spinner("Generating..."):
+                #model_input = update_and_get_context(prompt, agent.memory)
                 response = RAG.run(context + "\n" + prompt)
                 end_time = time.time()
                 placeholder = st.empty()
@@ -99,6 +176,17 @@ if 'OPENAI_API_KEY' in st.session_state and st.session_state['OPENAI_API_KEY']:
         st.write(f"Response generated in {response_time:.2f} seconds.")
         message = {"role": "assistant", "content": full_response}
         conversation["messages"].append(message)
+
+    if 'run_button' in st.session_state and st.session_state.run_button == True:
+        st.session_state.running = True
+    else:
+        st.session_state.running = False
+
+    if st.button("New Conversation", disabled=st.session_state.running, key='run_button'):
+        # Clear chat messages
+        st.session_state.conversations = [{"title": "Default Conversation", "messages": [{"role": "assistant", "content": "How may I assist you today?"}]}]
+        # Trigger a rerun
+        st.rerun()
 else:
     # If the API key is not set, continue showing the API key selection UI
     st.write("Please select or enter an OpenAI API key to continue.")
